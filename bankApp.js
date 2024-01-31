62c28f066c44e29f8b1bc87c4402bda3aa53ecd7
Saimon8420 <latifulkabir567@gmail.com>
2024-02-01 00:42:00 +0600

    sets readline for getting users input

--- bankApp.js
+++ bankApp.js
@@ -1,5 +1,17 @@
+// to receive users input via terminal
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdin,
+})
+
+
+// main function
 function main() {
-    console.log("\n1. Create a new account");
+    console.log("/n");
+    console.log("------------------*****-----------------");
+    console.log("1. Create a new account");
     console.log("2. Display all accounts");
     console.log("3. Update an account");
     console.log("4. Delete an account");
@@ -7,6 +19,8 @@ function main() {
     console.log("6. Withdraw an amount from your account");
     console.log("7. Search for account");
     console.log("8. Exit");
+    console.log("-----------------*****-----------------");
+    console.log("/n");
 }
 
 main();
