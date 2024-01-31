cff616438749f5b6371928c10c2f5b22a88aba54
Saimon8420 <latifulkabir567@gmail.com>
2024-02-01 01:28:21 +0600

    funtion createAccount added

--- bankApp.js
+++ bankApp.js
@@ -3,14 +3,64 @@ const readline = require("readline");
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdin,
+    output: process.stdout,
 })
+
+// stores data
+let accounts = [];
+
+function createAccount() {
+    try {
+        rl.question('Enter account holder\'s name: ', (name) => {
+            rl.question('Enter account number: ', (number) => {
+                rl.question('Enter account type (Savings/Current/Salary/Student): ', (type) => {
+                    rl.question('Enter initial balance: ', (balance) => {
+                        balance = parseFloat(balance);
+
+                        // matching account type exists of not
+                        const accountType = ["Savings", "Current", "Salary", "Student"];
+                        const matchedType = accountType.find(each => each.toLowerCase() === type.toLowerCase());
+
+                        // matching duplication of accountNo
+                        const matchedAccNo = accounts.find(each => each.number === number);
+                        if (balance < 500 || !matchedType || (accounts.length !== 0 && matchedAccNo)) {
+                            if (balance < 500) {
+                                console.log("\nInitially, You have to deposit minimum 500!!");
+                            }
+                            if (!matchedType) {
+                                console.log("\nAccount type not matched!!");
+                            }
+                            if (matchedAccNo) {
+                                console.log(`\nAccount number ${number} already exists!!`);
+                            }
+                            main();
+                        }
+
+                        else {
+                            let account = {
+                                name: name,
+                                number: number,
+                                balance: balance,
+                                creationDate: new Date(),
+                                type: type.toLowerCase(),
+                            };
+                            accounts.push(account);
+                            console.log(`\nCongrats!!Account ${number} created successfully`);
+                            main();
+                        }
+                    });
+                });
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 // main function
 function main() {
-    console.log("/n");
-    console.log("------------------*****-----------------");
+    console.log("\n------------------*****-----------------");
     console.log("1. Create a new account");
     console.log("2. Display all accounts");
     console.log("3. Update an account");
@@ -19,8 +69,22 @@ function main() {
     console.log("6. Withdraw an amount from your account");
     console.log("7. Search for account");
     console.log("8. Exit");
-    console.log("-----------------*****-----------------");
-    console.log("/n");
+    console.log("-----------------*****-----------------\n");
+
+    rl.question('Enter your choice: ', (choice) => {
+        switch (parseInt(choice)) {
+            case 1:
+                createAccount();
+                break;
+            case 8:
+                console.log("Exiting...Thank You!");
+                rl.close();
+                break;
+            default:
+                console.log("Invalid choice");
+                rl.close();
+        }
+    });
 }
 
 main();
