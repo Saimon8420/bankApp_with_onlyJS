3bb58a4bd1de43af10e79b0e7c4fc5bc4aa87c98
Saimon8420 <latifulkabir567@gmail.com>
2024-01-31 15:07:59 +0600

    asking criteria added into main

--- bankApp.js
+++ bankApp.js
@@ -1,1 +1,12 @@
-console.log("Bank app");
+function main() {
+    console.log("\n1. Create a new account");
+    console.log("2. Display all accounts");
+    console.log("3. Update an account");
+    console.log("4. Delete an account");
+    console.log("5. Deposit an amount into your account");
+    console.log("6. Withdraw an amount from your account");
+    console.log("7. Search for account");
+    console.log("8. Exit");
+}
+
+main();
